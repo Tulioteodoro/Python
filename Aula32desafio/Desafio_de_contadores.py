aecc56d3ea53f2adb6f4a031b3fbf4ab1f0ac5ad
Tulioteodoro <tulioredes@gmail.com>
2022-01-08 17:39:16 -0300

    Aula desafio - Contador
    Tulio iniciando no git

---
+++ Aula32desafio/Desafio_de_contadores.py
@@ -0,0 +1,32 @@
+""""""
+"""
+for / while
+0 10
+1 9
+2 8
+3 7
+4 6
+5 5
+6 4
+7 3
+8 2
+"""
+
+
+
+cont1 = ''
+cont2 = ''
+
+
+# while cont1 < 8:
+#     print(cont1,cont2)
+#
+#     cont1 +=1
+#     cont2 -=1
+# else:
+#     print(cont1, cont2)
+#
+
+
+for r in range(10,1,-1):
+    print(r)
